5c70c264ebd32013a0121d298de5ff9039323d3e
Seiya830 <seiya@MacBook-Pro.local>
2023-10-03 11:17:11 +0900

    add メモ新規登録機能追加

--- backend/src/routes/articles/articles.router.js
+++ backend/src/routes/articles/articles.router.js
@@ -8,19 +8,20 @@ const router = express.Router();
 /**
  * メモ新規登録
  */
-router.post('/add', authenticate, async (req, res, next) => {
+router.post('/add', async (req, res, next) => {
   try {
     // リクエストボディからメモデータを取得
-    const { title, body, author_id } = req.body;
+    const { title, content, author_id } = req.body;
 
     // データベースに新しいメモを作成して保存
     const newMemo = await db.Articles.create({
       title,
-      content: body,
+      content,
       author_id,
     });
 
-    res.status(201).json(newMemo); // 新しく作成されたメモを返す
+    // 新しく作成されたメモを返す
+    res.status(201).json(newMemo);
   } catch (error) {
     console.error(error);
     res.status(500).json({});
--- frontend/src/pages/MemoInput.vue
+++ frontend/src/pages/MemoInput.vue
@@ -1,53 +1,46 @@
-<script lang="ts">
-export default {
-  data() {
-    return {
-      title: "",
-      content: "",
-      author_id: "",
-      memos: [] as { title: string; content: string }[], // メモを格納するリスト
-    };
-  },
-  methods: {
-    async addMemo() {
-      try {
-        // 新しいメモを作成するためのPOSTリクエストを送信
-        const response = await fetch("/add", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            title: this.title,
-            body: this.content,
-          }),
-        });
+<script setup lang="ts">
+// Vue3のComposition APIから必要な関数をインポート
+import { ref } from "vue";
 
-        if (response.ok) {
-          // 入力フィールドをクリア
-          this.title = "";
-          this.content = "";
+// 環境変数（.env参照）からAPIのベースURLを取得
+const $config = useRuntimeConfig();
+const apiBaseUrl = $config.public.apiBaseUrl;
 
-          const newMemo = await response.json();
-          this.memos.push(newMemo);
-        } else {
-          console.error("メモの作成中にエラーが発生しました");
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  },
+// メモのデータを格納するリアクティブな変数を作成
+const memoData = ref({
+  title: "",
+  content: "",
+  author_id: 1,
+});
+
+// メモを保存する非同期関数を定義
+const saveMemo = async () => {
+  try {
+    // メモをAPIに送信して保存
+    const { data } = await useFetch(`${apiBaseUrl}/articles/add`, {
+      method: "POST",
+      body: {
+        title: memoData.value.title,
+        content: memoData.value.content,
+        author_id: memoData.value.author_id,
+      },
+    });
+    // 保存が成功した場合、APIからの応答データをログに表示
+    console.log(data);
+  } catch (error) {
+    // エラーが発生した場合、エラーをログに表示
+    console.log(error);
+  }
 };
 </script>
 
 <template>
   <div>
-    <form @submit.prevent="addMemo">
+    <form @submit.prevent="saveMemo">
       <p>タイトル</p>
-      <input type="text" v-model="title" placeholder="メモタイトル" required />
+      <input type="text" v-model="memoData.title" placeholder="メモタイトル" />
       <p>本文</p>
-      <textarea v-model="content" placeholder="本文" required></textarea>
+      <textarea v-model="memoData.content" placeholder="本文"></textarea>
 
       <button type="submit">追加</button>
     </form>
