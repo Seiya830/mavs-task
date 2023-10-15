4341860ff8d80dd7fcbcbc53b7da83e121e52953
Seiya830 <seiya@MacBook-Pro.local>
2023-10-16 06:29:58 +0900

    add リンク、scss追加

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,7 +1,47 @@
 <template>
-  <div>
-    <h2>TOP</h2>
+  <div class="app-container">
+    <h2>メモアプリ</h2>
+    <div class="link-container">
+      <nuxt-link to="/signin" class="link-button">サインイン</nuxt-link>
+      <nuxt-link to="/signup" class="link-button">新規登録</nuxt-link>
+    </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 50px;
+
+  h2 {
+    margin-bottom: 16px;
+  }
+}
+
+.link-container {
+  .link-button {
+    display: inline-block;
+    margin-bottom: 10px;
+    margin-right: 10px;
+    padding: 10px 20px;
+    font-size: 16px;
+    color: #fff;
+    background-color: #007bff;
+    border: none;
+    border-radius: 5px;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+
+    &:last-child {
+      margin-right: 0;
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
